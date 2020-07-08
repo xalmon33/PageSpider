fec7d3179a9b98f74c7dddd69edd1292f05a1912
Calvin Sellers <dusstee823@gmail.com>
2020-07-07 21:56:58 -0400

    Initial Commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_file(file_path: str):
+    #TODO: add code to read text file with urls
+    pass
+
+def load_page(url: str):
+    #TODO: add code to read urls
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: analyze text
+    pass
